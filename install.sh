8f0b151b310f0056e54a9fab79c5beedac9de0b2
Major Hayden <major@mhtx.net>
2020-05-07 16:17:29 -0500

    More fixes

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -euxo pipefail
 
 # Install Ansible
 pip -q install ansible
@@ -8,4 +7,4 @@ pip -q install ansible
 ssh-keygen -b 4096 -t rsa -f /tmp/sshkey -q -N ""
 mkdir -vp ~/.ssh && chmod 0700 ~/.ssh
 cat /tmp/sshkey.pub >> ~/.ssh/authorized_keys && chmod 0700 ~/.ssh/authorized_keys
-sudo systemctl enable --now ssh
+sudo systemctl start ssh
