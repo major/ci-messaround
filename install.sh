9e69cd1b4ed601eea67d1e6058ba5712a1579dca
Major Hayden <major@mhtx.net>
2020-05-07 16:11:49 -0500

    Re-arrange stuff

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euxo pipefail
+
+# Install Ansible
+pip -q install ansible
+
+# Set up ssh keys and ssh daemon.
+ssh-keygen -b 4096 -t rsa -f /tmp/sshkey -q -N ""
+mkdir -vp ~/.ssh && chmod 0700 ~/.ssh
+cat /tmp/sshkey.pub >> ~/.ssh/authorized_keys && chmod 0700 ~/.ssh/authorized_keys
+sudo systemctl enable --now ssh
