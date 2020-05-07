1bdbb1fe78cb57883f454d2a8724823ccfc2b4b5
Major Hayden <major@mhtx.net>
2020-05-07 13:59:27 -0500

    Install cpuchecker

--- run_tests.sh
+++ run_tests.sh
@@ -4,8 +4,8 @@ set -euxo pipefail
 FEDORA_MIRROR=http://mirror.lstn.net/fedora/releases/32/Cloud/x86_64/images
 FEDORA_QCOW=Fedora-Cloud-Base-32-1.6.x86_64.qcow2
 
-sudo apt update
-sudo apt -y install aria2 bridge-utils libguestfs-tools \
+sudo apt-get -q update
+sudo apt-get -qy install aria2 bridge-utils cpu-checker libguestfs-tools \
     libvirt-bin libvirt-clients libvirt-daemon libvirt-daemon-system \
     kmod qemu-kvm wget
 
